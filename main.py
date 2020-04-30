02fe56c30120661d12d10cd22fc0d4e88d223770
Kevin Wei Yu <kuanweiyu@gmail.com>
2020-04-30 07:34:20 -0400

    update chrome options

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from selenium import webdriver
 import os
 import time
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), 
+	chrome_options=chrome_options)
 
 SLEEP_TIME = 4
 
